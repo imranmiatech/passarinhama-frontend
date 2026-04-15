40ea34cd6b611d21128bf8e4e319f39eb6faf967
imranmiatech <imran32472@gmail.com>
2026-04-15 22:54:13 +0600

    ok

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,11 @@
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="w-full py-5 flex justify-between px-5 bg-gray-300">
+      <p>Tableau de bord</p>
+      <div>
+        <input type="text" placeholder="Search here"/>
+      </div>
+    </div>
   )
 }
